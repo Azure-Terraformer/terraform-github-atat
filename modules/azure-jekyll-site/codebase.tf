1a74ad79c6d46a7ec8cb92b8a9a77602f0779cbb
markti <markti@outlook.com>
2025-06-15 16:45:42 -0400

    oops

--- modules/azure-jekyll-site/codebase.tf
+++ modules/azure-jekyll-site/codebase.tf
@@ -1,7 +1,7 @@
 module "codebase" {
 
   source  = "Azure-Terraformer/codebase-terraform-azure-jekyll-site/github"
-  version = "1.0.15"
+  version = "1.0.17"
 
   repository   = var.repository_name
   branch       = github_branch.main.branch
