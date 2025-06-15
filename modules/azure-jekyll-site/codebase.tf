4d884e2a82b44e2592cbf4eefad152fc5b885a96
markti <markti@outlook.com>
2025-06-15 16:12:46 -0400

    v13

--- modules/azure-jekyll-site/codebase.tf
+++ modules/azure-jekyll-site/codebase.tf
@@ -1,7 +1,7 @@
 module "codebase" {
 
   source  = "Azure-Terraformer/codebase-terraform-azure-jekyll-site/github"
-  version = "1.0.11"
+  version = "1.0.13"
 
   repository   = var.repository_name
   branch       = github_branch.main.branch
